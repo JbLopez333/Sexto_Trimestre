f424cf854984770f8fbc93ea21f730ec0faa9aa1
Jeremy Brandon López Velandia <jeremyl1524v@gmail.com>
2026-03-21 17:21:54 +0000

    Listas

--- MiPrimerAngular/src/app/app.ts
+++ MiPrimerAngular/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('MiPrimerAngular');
+  Bienvenidos = 'Hola Jeremy'
+
+  Estudiantes = ['Cuadernos','Gorras','Gafas']
+
 }
