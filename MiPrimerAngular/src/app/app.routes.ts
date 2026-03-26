26581c57ae31c2beeffd688aac07f41a8f6c1323
Jeremy Brandon López Velandia <jeremyl1524v@gmail.com>
2026-03-26 02:02:59 +0000

    Creacion de Pages: inicio, contactos , ejercicios

--- MiPrimerAngular/src/app/app.routes.ts
+++ MiPrimerAngular/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {} from './pages/inicio/inicio';
+import {} from './pages/contactos/contactos';
+import {} from './pages/ejercicios/ejercicios';
 
 export const routes: Routes = [];
